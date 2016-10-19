ae5646a8417aeebb720a0637bb4cd933c70d5577
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2016-10-19 11:25:22 +0200

    Move test input and output files to subdirectory.
    Move test input and output files to subdirectory.
    
    Signed-off-by: Gert Hulselmans <gert.hulselmans@kuleuven.be>

--- compile.sh
+++ compile.sh
@@ -10,5 +10,5 @@ javac OrderStatistics.java
 jar cvfm OrderStatistics.jar manifest.txt *.class
 
 # Test if new version generates the same output than the old one.
-java -jar ./OrderStatistics.jar *.rr | sort -k2,2g -k1,1 > new.r
-diff -u new.r old.r
+java -jar ./OrderStatistics.jar test_files/rankratios/*.rr | LC_ALL=C sort -k2,2g -k1,1 > test_files/rankings/new.r
+diff -u test_files/rankings/new.r test_files/rankings/old.r
